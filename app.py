0c6ff1e9e8c293504229fc686dc8cfccc00010b5
KimHyeonBin123 <qwe4800@gmail.com>
2025-08-26 19:29:55 +0900

    Update app.py_v_0.1.8

--- app.py
+++ app.py
@@ -1,83 +1,61 @@
-# --- Helper: 아이콘 URL 생성 ---
-def get_icon_url(name, img_dict, category="item"):
-    img_file = img_dict.get(name)
-    if not img_file:
-        return None
-    if category == "item":
-        return f"http://ddragon.leagueoflegends.com/cdn/13.17.1/img/item/{img_file}"
-    elif category == "spell":
-        return f"http://ddragon.leagueoflegends.com/cdn/13.17.1/img/spell/{img_file}"
-    elif category == "rune":
-        return f"http://ddragon.leagueoflegends.com/cdn/img/{img_file}"
-    return None
+# app.py
+import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 
-# --- Recommended Items ---
-st.subheader('Recommended Items')
-items = compute_item_stats(champ_df)
-items['item_icon'] = items['item'].apply(lambda x: get_icon_url(x, item_name_to_img, "item"))
+# 샘플 데이터 (URL은 실제 아이콘 이미지 주소로 교체하세요)
+data = {
+    "champion": ["Ahri", "Yasuo", "Lux"],
+    "spell1": [
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerFlash.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerHeal.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerDot.png"
+    ],
+    "spell2": [
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerHeal.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerFlash.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/spell/SummonerBarrier.png"
+    ],
+    "item1": [
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/6655.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/3031.png",
+        "https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/3285.png"
+    ],
+}
 
-gb = GridOptionsBuilder.from_dataframe(items)
-gb.configure_default_column(filterable=True, sortable=True, resizable=True)
-gb.configure_column(
-    "item_icon",
-    header_name="Item",
-    cellRenderer="""
-    function(params) {
-        if (params.value) {
-            return `<img src="${params.value}" style="width:20px;height:20px;vertical-align:middle;margin-right:4px;">`;
+df = pd.DataFrame(data)
+
+st.title("칼바람 챔피언 추천 빌드")
+st.subheader("Recommended Items & Spells")
+
+# Grid 설정
+gb = GridOptionsBuilder.from_dataframe(df)
+
+# 챔피언 이름은 텍스트 그대로
+gb.configure_column("champion", header_name="Champion")
+
+# 스펠/아이템은 이미지로 렌더링
+for col in ["spell1", "spell2", "item1"]:
+    gb.configure_column(
+        col,
+        header_name=col.capitalize(),
+        cellRenderer='''
+        function(params) {
+            if (params.value) {
+                return `<img src="${params.value}" width="30" height="30"/>`
+            }
+            return "";
         }
-        return '';
-    }
-    """,
-    width=100
+        '''
+    )
+
+grid_options = gb.build()
+
+# AgGrid 출력
+AgGrid(
+    df,
+    gridOptions=grid_options,
+    allow_unsafe_jscode=True,  # JS 코드 허용해야 이미지 렌더링됨
+    height=300,
+    fit_columns_on_grid_load=True,
 )
-gridOptions = gb.build()
-AgGrid(items, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
-
-# --- Recommended Spells ---
-st.subheader('Recommended Spell Combos')
-spells = compute_spell_stats(champ_df)
-spells['spell1_icon'] = spells['spell1'].apply(lambda x: get_icon_url(x, spell_name_to_img, "spell"))
-spells['spell2_icon'] = spells['spell2'].apply(lambda x: get_icon_url(x, spell_name_to_img, "spell"))
-
-gb = GridOptionsBuilder.from_dataframe(spells)
-gb.configure_default_column(filterable=True, sortable=True, resizable=True)
-for col in ["spell1_icon","spell2_icon"]:
-    gb.configure_column(
-        col,
-        cellRenderer="""
-        function(params) {
-            if (params.value) {
-                return `<img src="${params.value}" style="width:20px;height:20px;vertical-align:middle;margin-right:4px;">`;
-            }
-            return '';
-        }
-        """,
-        width=100
-    )
-gridOptions = gb.build()
-AgGrid(spells, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
-
-# --- Recommended Runes ---
-st.subheader('Recommended Rune Combos')
-runes = compute_rune_stats(champ_df)
-runes['rune_core_icon'] = runes['rune_core'].apply(lambda x: get_icon_url(x, rune_name_to_img, "rune"))
-runes['rune_sub_icon'] = runes['rune_sub'].apply(lambda x: get_icon_url(x, rune_name_to_img, "rune"))
-
-gb = GridOptionsBuilder.from_dataframe(runes)
-gb.configure_default_column(filterable=True, sortable=True, resizable=True)
-for col in ["rune_core_icon","rune_sub_icon"]:
-    gb.configure_column(
-        col,
-        cellRenderer="""
-        function(params) {
-            if (params.value) {
-                return `<img src="${params.value}" style="width:20px;height:20px;vertical-align:middle;margin-right:4px;">`;
-            }
-            return '';
-        }
-        """,
-        width=100
-    )
-gridOptions = gb.build()
-AgGrid(runes, gridOptions=gridOptions, enable_enterprise_modules=False, fit_columns_on_grid_load=True)
